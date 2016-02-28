ad408b3edfad59d8859ac28ca8528b98fb9758d2
Yiwen Xu <xuywformal@gmail.com>
2016-02-28 13:01:14 -0600

    compare three method

--- MP2_Scale-space_Blob_Detection/run.m
+++ MP2_Scale-space_Blob_Detection/run.m
@@ -1,12 +1,12 @@
 %% Setup environment and read in images
 close all; clear; clc;
 
-img_id = 4;
+img_id = 1;
 sigma_id =  {2,     2,      2,      2};
-maxR_id =   {50,    60,     55,     55};
+maxR_id =   {55,    60,     55,     55};
 n_id =      {10,    10,     10,     10};
 rad_id =    {2,     2,      2,      3};
-pct_id =    {0.03,  0.03,   0.12,   0.15};
+pct_id =    {0.04,  0.03,   0.12,   0.15};
 
 img_addr = ['imgs/', sprintf('%02d',img_id), '.jpg'];
 img = im2double(rgb2gray(imread(img_addr)));
@@ -17,24 +17,40 @@ img = im2double(rgb2gray(imread(img_addr)));
 sigma = sigma_id{img_id};                           % Initial Sigma Size
 maxR = maxR_id{img_id};                             % Max Region to Detect
 n = n_id{img_id};                                   % Iterative Times
+spc_mtd = 'sub';
 k = nthroot(maxR/sqrt(2)/sigma,n);                  % Kernel Factor
 display = true;                                     % Show plots
 
-[img_space,scl_space] = up_kernel(img,sigma,k,n,display);
-% [img_space,scl_space] = sub_figure(img,sigma,k,n,display);
+if (strcmp(spc_mtd,'upk'))
+    [img_space,scl_space] = up_kernel(img,sigma,k,n,display);
+elseif (strcmp(spc_mtd,'sub')) 
+    [img_space,scl_space] = sub_figure(img,sigma,k,n,display);
+else error('Wrong Method');  
+end
 
 %% Nonmaximun Suppression and Scale-fitting
 % Settings
 rad = rad_id{img_id};                               % Radius in Nonmaximum suppression(1~3);
 pct = pct_id{img_id};                               % Threshold in max precentage
+maxi_mtd = 'ordfilt2';                              % Method: 'ordfilt2', 'colfilt', 'nlfilter'
 th = pct * max(img_space(:));                       % Threshold response
 
 % Slidewise Nonmaximum Suppression
 maxi_space = img_space;
+sz = 2*rad+1;
+t_start = tic;
 for i = 1:n
-    sz = 2*rad+1;
-	maxi_space(:,:,i) = ordfilt2(img_space(:,:,i),sz^2,ones(sz));
+	if (strcmp(maxi_mtd,'ordfilt2')) 
+        maxi_space(:,:,i) = ordfilt2(img_space(:,:,i),sz^2,ones(sz));
+    elseif (strcmp(maxi_mtd,'colfilt')) 
+        maxi_space(:,:,i) = colfilt(img_space(:,:,i),[sz,sz],'sliding',@max); 
+    elseif (strcmp(maxi_mtd,'nlfilter')) 
+        fun = @(x) max(x(:));
+        maxi_space(:,:,i) = nlfilter(img_space(:,:,i),[sz,sz],fun); 
+    end
 end
+t = toc(t_start);
+fprintf('Running Time - %s: %6.6f s\n',maxi_mtd,t);
 
 % Third Dimension Nonmaximum Suppression
 x =[]; y = []; r = [];
--- MP2_Scale-space_Blob_Detection/sub_figure.m
+++ MP2_Scale-space_Blob_Detection/sub_figure.m
@@ -1,19 +1,21 @@
 function [img_space,scl_space] = sub_figure(img,sigma,k,n,display)
-    [h,w,~] = size(img);
+    [h,w] = size(img);
     img_space = zeros(h,w,n);
     scl_space = zeros(n,1);scl_space(1) = 1;
     
     t_start = tic;
-    lap = fspecial('log',7*sigma,sigma);
+    lap = fspecial('log',6*sigma,sigma);
     nor_lap = lap.*(sigma^2);
+    sig = 1;
     for i = 1:n
-        ims = imresize(img,1/scl_space(i),'Antialiasing',true);
+        ims = imresize(img,1/sig,'Antialiasing',true);
         imf = imfilter(ims,nor_lap,'symmetric');
         imf = imf.^2;
         imu = imresize(imf,[h,w],'bicubic');
+        sig = k^i;
 
         img_space(:,:,i) = imu;
-        scl_space(i+1) = scl_space(i)*k;
+        scl_space(i+1) = sig;
     end
     t = toc(t_start);
     fprintf('Running Time - Subsampled Image: %6.6f s\n',t);
