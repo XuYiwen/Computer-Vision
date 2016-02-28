047d73d0dbc0d274a9f0c9d02d8605c94f6feb4d
Yiwen Xu <xuywformal@gmail.com>
2016-02-27 23:51:41 -0600

    logical bug fixed, first image tuned.

--- MP2_Scale-space_Blob_Detection/run.m
+++ MP2_Scale-space_Blob_Detection/run.m
@@ -1,50 +1,57 @@
-%% Setup environment and read test image
+%% Setup environment and read in images
 close all; clear; clc;
-img_id = 1;
-img_addr = ['imgs/', sprintf('%02d',img_id), '.jpg'];
 
+img_id = 1;
+sigma_id =  {2,     2,      2,      2};
+maxR_id =   {50,    50,     50,     50};
+n_id =      {10,    10,     10,     10};
+rad_id =    {2,     2,      2,      2};
+pct_id =    {0.03,  0.03,   0.03,   0.03};
+
+img_addr = ['imgs/', sprintf('%02d',img_id), '.jpg'];
 img = im2double(rgb2gray(imread(img_addr)));
 [h,w,~] = size(img);
-% figure(),imshow(img);
 
 %% Upsampling Kernel Size
 % Settings
-sigma = 2;                      % Initial Sigma Size
-maxR = 30;                      % Max Region to Detect
-n = 10;                         % Iterative Times
-k = nthroot(maxR/sqrt(2),n);    % Kernel Factor
-display = true;                 % Show plots
+sigma = sigma_id{img_id};                           % Initial Sigma Size
+maxR = maxR_id{img_id};                             % Max Region to Detect
+n = n_id{img_id};                                   % Iterative Times
+k = nthroot(maxR/sqrt(2)/sigma,n);                  % Kernel Factor
+display = true;                                     % Show plots
 
-% [img_space,scl_space] = up_kernel(img,sigma,k,n,display);
-[img_space,scl_space] = sub_figure(img,sigma,k,n,display);
+[img_space,scl_space] = up_kernel(img,sigma,k,n,display);
+% [img_space,scl_space] = sub_figure(img,sigma,k,n,display);
 
 %% Nonmaximun Suppression and Scale-fitting
 % Settings
-rad = 2;                        % Radius in Nonmaximum suppression(1~3);
-th = 0.02 * max(img_space(:));  % Threshold response
+rad = rad_id{img_id};                               % Radius in Nonmaximum suppression(1~3);
+pct = pct_id{img_id};                               % Threshold in max precentage
+th = pct * max(img_space(:));                       % Threshold response
 
 % Slidewise Nonmaximum Suppression
+maxi_space = img_space;
 for i = 1:n
-    im = img_space(:,:,i);
-    sz = 2*rad+1;                            % Size of mask.
-	maxi = ordfilt2(im,sz^2,ones(sz));       % Grey-scale dilate.
-	im = (im==maxi)&(im>th);                 % Find maxima.
-    img_space(:,:,i) = im;
+    sz = 2*rad+1;
+	maxi_space(:,:,i) = ordfilt2(img_space(:,:,i),sz^2,ones(sz));
 end
 
 % Third Dimension Nonmaximum Suppression
 x =[]; y = []; r = [];
-for i = 2:n-1
-    up = img_space(:,:,i-1);
-    im = img_space(:,:,i);
-    down = img_space(:,:,i+1);
-    mask = (im-up>0) & (im-down>0);
+for i = 1:n
+    cur = maxi_space(:,:,i);
+    cur_up = ones(size(cur)); cur_down = ones(size(cur));
+    if (i>1) up = maxi_space(:,:,i-1); cur_up = (cur-up)>0; end
+    if (i<n) down = maxi_space(:,:,i+1); cur_down = (cur-down)>0; end
+    at_max_level = (cur_up) & (cur_down);
     
-    [cx,cy] = find(mask); 
+    im = img_space(:,:,i); 
+    at_max_center = (im == cur) & (im > th);
+    
+    cim = at_max_level .* at_max_center;
+    
+    [cx,cy] = find(cim); 
     cr = ones(size(cx)).*scl_space(i);
     x = [x;cx]; y = [y;cy]; r = [r;cr];
 end
 figure(4),show_all_circles(img, y, x, r);
-set(gcf,'position',[1 500 2*w 2*h]);
-
-
--- MP2_Scale-space_Blob_Detection/up_kernel.m
+++ MP2_Scale-space_Blob_Detection/up_kernel.m
@@ -4,15 +4,16 @@ function [img_space,scl_space] = up_kernel(img,sigma,k,n,display)
     scl_space = zeros(n,1);scl_space(1) = sigma;
     
     t_start = tic;
+    sig = sigma;
     for i = 1:n
-        lap = fspecial('log',7*sigma,sigma);
-        nor_lap = lap.*(sigma^2);
+        lap = fspecial('log',6*sig,sig);
+        nor_lap = lap.*(sig^2);
         imf = imfilter(img,nor_lap,'symmetric');
         imf = imf.^2;
-        sigma = round(sigma*k);
+        sig = round(sigma*(k^i));
         
         img_space(:,:,i) = imf;
-        scl_space(i+1) = sigma; % actually you dont need this 
+        scl_space(i+1) = sig; % actually you dont need this 
     end
     t = toc(t_start);
     fprintf('Running Time - Upsampled Kernel: %6.6f s\n',t);
